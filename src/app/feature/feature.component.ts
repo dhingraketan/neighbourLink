4b71a8969da623cb4e7cfe9deb9ff0149a1c948b
Ketan Dhingra <kda59@sfu.ca>
2024-12-04 14:46:04 -0800

    Download buttons added and video links added

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class FeatureComponent {
 
+  openAPK(){
+    window.open('https://drive.google.com/file/d/1MWvxCaw2KoVy5VcHftOd34Rp6M6JNbB9/view?usp=share_link', '_blank');
+  }
+
+  openZIP(){
+    window.open('https://drive.google.com/file/d/1304L6ApgUTyVV-nG-0CH8B4na9I7kXIM/view?usp=share_link', '_blank');
+  }
+
 }
